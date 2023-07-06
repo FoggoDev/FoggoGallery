ca6573620e2a67ffa0d77816cf37d6a5b2f02603
DoggoNako <sashapoun@gmail.com>
2023-07-06 21:47:39 +0200

    [HTML] separate desktop and phone nav (for easier testing) [CSS] adapatation for phone added (not finished) [JS] script.js adapted (opening and closing menu on phone) [Files] two icons added (menu.svg & x.svg)

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const socialsDiv = document.querySelector('.socials');
-const socialBtn = document.querySelector('nav .share');
+document.addEventListener("DOMContentLoaded", function () {
+    const socialsDiv = document.querySelector('.socials');
+    const socialBtn = document.querySelector('nav .share');
+    const mobileNavbar = document.querySelector('nav.phone subnav');
 
-socialBtn.addEventListener('mouseenter', () => { 
-    socialsDiv.style.left = `${socialBtn.getBoundingClientRect().left - 8}px`
-    socialsDiv.classList.replace('hide', 'show') 
-})
-socialsDiv.addEventListener('mouseleave', () => { socialsDiv.classList.replace('show', 'hide') })
+    socialBtn.addEventListener('mouseenter', () => { 
+        socialsDiv.style.left = `${socialBtn.getBoundingClientRect().left}px`
+        socialsDiv.classList.replace('hide', 'show') 
+    })
+    socialsDiv.addEventListener('mouseleave', () => { socialsDiv.classList.replace('show', 'hide') })
+    
+    document.querySelector('nav.phone > img').addEventListener('click', () => { mobileNavbar.style.display = 'flex' })
+    document.querySelector('nav.phone subnav .title > img').addEventListener('click', () => { mobileNavbar.style.display = 'none' })
+});
+
