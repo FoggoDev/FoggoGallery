917d0e37c24e8319e738533636c03b17df2b8100
DoggoNako <sashapoun@gmail.com>
2023-07-07 12:12:24 +0200

    New Files, index.html, css and js modified [HTML] simplified nav for phone (need to add socials net at the bottom & make it for all the website) [CSS] modified to welcome the new adapted nav, icons behind each hypertext in the list of nav, and some transition for the subnav [JS] Adapted to make all transition work

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
+    /* Only Desktop */
     const socialsDiv = document.querySelector('.socials');
     const socialBtn = document.querySelector('nav .share');
-    const mobileNavbar = document.querySelector('nav.phone subnav');
 
     socialBtn.addEventListener('mouseenter', () => { 
         socialsDiv.style.left = `${socialBtn.getBoundingClientRect().left}px`
-        socialsDiv.classList.replace('hide', 'show') 
+        socialsDiv.classList.replace('fadeout', 'fadein') 
     })
-    socialsDiv.addEventListener('mouseleave', () => { socialsDiv.classList.replace('show', 'hide') })
+    socialsDiv.addEventListener('mouseleave', () => { socialsDiv.classList.replace('fadein', 'fadeout') })
+    /* END */
+
+    /* Only Mobile */
+    const mobileNavbar = document.querySelector('nav.phone subnav');
+    const mobileNavbarBtn = document.querySelector('nav.phone > img');
+    const mobileNavBarList = document.querySelector('nav.phone subnav ul');
+
+    mobileNavbarBtn.addEventListener('click', () => { 
+        if (mobileNavbarBtn.getAttribute("src") === 'image/icon/menu.svg') { 
+            mobileNavbarBtn.setAttribute('src', 'image/icon/x.svg')
+            mobileNavbar.classList.replace('slideout', 'slidein')
+            mobileNavBarList.classList.replace('fadeout', 'fadein')
+        } else { 
+            mobileNavbarBtn.setAttribute('src', 'image/icon/menu.svg')
+            mobileNavbar.classList.replace('slidein', 'slideout')
+            mobileNavBarList.classList.replace('fadein', 'fadeout')
+        }
+    })
+    /* END */
+
+
     
-    document.querySelector('nav.phone > img').addEventListener('click', () => { mobileNavbar.style.display = 'flex' })
-    document.querySelector('nav.phone subnav .title > img').addEventListener('click', () => { mobileNavbar.style.display = 'none' })
+
 });
 
